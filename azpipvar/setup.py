f30cc9bf3c96a5b4791ae706e21cb54024cdc9ba
greatbody <sunruicode@gmail.com>
2025-02-20 00:44:20 +0800

    Bump package version to 0.1.5

--- azpipvar/setup.py
+++ azpipvar/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, Command
 from setuptools.command.install import install
 import sys
 
-version = "0.1.4"
+version = "0.1.5"
 
 class CustomInstallCommand(install):
     def run(self):
